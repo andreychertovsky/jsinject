13235657517661198c0f471505371610e5218f1f
andreychertovsky <andreychertovsky88@gmail.com>
2017-08-04 15:32:55 +0300

    Update bg.js

--- bg.js
+++ bg.js
@@ -1,32 +1,32 @@
 const deepLink = 'http://shopeasy.by/redirect/cpa/o/ou5s9n6tefplqafc1x0mef29smqnwywq/';
 
-function madeReq(){
-  return new Promise(function(resolve, reject){
-    chrome.webRequest.onBeforeRequest.addListener(function(e){
-     if (urlMatch(e.url)){
-        const encode = encodeURIComponent(urlMatch(e.url));
-        const composed = compose(deepLink, encode)
+function madeReq(){ //основная функция
+  return new Promise(function(resolve, reject){ // создает объект промис
+    chrome.webRequest.onBeforeRequest.addListener(function(e){ //вызов хром апи, запускается перед реквесто
+     if (urlMatch(e.url)){ //проверка на true
+        const encode = encodeURIComponent(urlMatch(e.url)); //строка полученная из функции urlMatch кодируется urlEncod ом
+        const composed = compose(deepLink, encode) // возвращает готовую ссылку пригодную для перехода
         console.log(composed);
-        resolve(composed)
+        resolve(composed) //возвращает ссылку
       }
     }, {urls: ["<all_urls>"], types: ['main_frame', 'sub_frame']},["blocking"]);
   })
 }
 
-madeReq().then(function(comp){
+madeReq().then(function(comp){ //промис
   console.log(comp);
-  chrome.tabs.update(undefined, {url:comp});
+  chrome.tabs.update(undefined, {url:comp}); //подставляет готовую ссылку партнерки и переходит на нее  текущей вкладке
 }, function(err){
-  console.log(err);
+  console.log(err); //лог ошибок
 });
 
-function urlMatch(url){
-  const reg = /^http(.*?)aliexpress(.*?)item(.*?html)/g
-  return reg.test(url)?url.match(reg)[0]:false
+function urlMatch(url){ // функция получает на вход url
+  const reg = /^http(.*?)aliexpress(.*?)item(.*?html)/g // шаблон для проверки url
+  return reg.test(url)?url.match(reg)[0]:false // возвращает строку типа *aliexpress*/item/*.html либо возвращает false
 }
 
-function compose(first, encode){
-  return first+'?to='+encode;
+function compose(first, encode){ //складывает строку
+  return first+'?to='+encode; //возвращает диплинк + строку полсе функции urlEncode
 }
 /*
 function likResult(){
